5379043b9bd843e4b94deab93df5f3a46785f2f0
diogofalken <diogofalken0@gmail.com>
2023-07-07 22:28:52 +0100

    feat: add recipe connection graphs

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EChartsOption } from 'echarts';
 import { ChoreographiesService } from '../../services/choreographies.service';
 import { SourcesService } from '../../services/sources.service';
-import { Statement } from '../../types/core.types';
+import { Recipe, Statement } from '../../types/core.types';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +10,8 @@ import { Statement } from '../../types/core.types';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  responseStatisticsOptions!: EChartsOption;
+  recipeStatisticsOptions!: EChartsOption;
+  recipeGraphOptions!: EChartsOption;
   choreographiesStatisticsOptions!: EChartsOption;
 
   choreographiesDistributionInTimeOptions!: EChartsOption;
@@ -26,7 +27,7 @@ export class DashboardComponent implements OnInit {
         next: (response) => {
           if (!response.recipe) return;
 
-          this.responseStatisticsOptions = {
+          this.recipeStatisticsOptions = {
             tooltip: {
               trigger: 'axis',
               axisPointer: {
@@ -64,6 +65,50 @@ export class DashboardComponent implements OnInit {
                   response.recipe.objects.length,
                   response.recipe.places.length,
                 ],
+              },
+            ],
+          };
+
+          const graphData = this.getGraphRecipeData(response.recipe);
+          this.recipeGraphOptions = {
+            title: {},
+            tooltip: {},
+            legend: [
+              {
+                // selectedMode: 'single',
+                data: graphData.categories.map(function (a: { name: string }) {
+                  return a.name;
+                }),
+              },
+            ],
+            animationDuration: 1500,
+            animationEasingUpdate: 'quinticInOut',
+            series: [
+              {
+                name: 'Generated Recipe',
+                type: 'graph',
+                layout: 'force',
+                data: graphData.nodes,
+                links: graphData.links,
+                categories: graphData.categories,
+                roam: true,
+                label: {
+                  position: 'right',
+                  formatter: '{b}',
+                },
+                lineStyle: {
+                  color: 'source',
+                  curveness: 0.3,
+                },
+                emphasis: {
+                  focus: 'adjacency',
+                  lineStyle: {
+                    width: 10,
+                  },
+                },
+                force: {
+                  repulsion: 100,
+                },
               },
             ],
           };
@@ -193,6 +238,145 @@ export class DashboardComponent implements OnInit {
       afternoon: afternoonCounters,
       evening: eveningCounters,
       night: nightCounters,
+    };
+  }
+
+  private getGraphRecipeData(recipe: Recipe) {
+    const nodes: {
+      id: string;
+      name: string;
+      value: number;
+      symbolSize?: number;
+      category: number;
+      label?: { show: true };
+    }[] = [
+      {
+        id: '0',
+        name: 'Actors',
+        value: 3,
+        symbolSize: 30,
+        category: 0,
+        label: { show: true },
+      },
+      {
+        id: '1',
+        name: 'Verbs',
+        value: 3,
+        symbolSize: 30,
+        category: 1,
+        label: { show: true },
+      },
+      {
+        id: '2',
+        name: 'Objects',
+        value: 3,
+        symbolSize: 30,
+        category: 2,
+        label: { show: true },
+      },
+      {
+        id: '3',
+        name: 'Places',
+        value: 3,
+        symbolSize: 30,
+        category: 3,
+        label: { show: true },
+      },
+    ];
+    let i = nodes.length;
+    const links = [
+      {
+        source: '0',
+        target: '1',
+      },
+      {
+        source: '1',
+        target: '2',
+      },
+      {
+        source: '2',
+        target: '3',
+      },
+      {
+        source: '3',
+        target: '0',
+      },
+    ];
+
+    for (const actor of recipe.actors) {
+      nodes.push({
+        id: actor.id,
+        name: actor.name,
+        value: 1,
+        symbolSize: 10,
+        category: 0,
+      });
+      links.push({
+        source: actor.id,
+        target: '0',
+      });
+    }
+    for (const verb of recipe.verbs) {
+      nodes.push({
+        id: verb.id,
+        name: verb.display,
+        value: 1,
+        symbolSize: 10,
+        category: 1,
+      });
+      links.push({
+        source: verb.id,
+        target: '1',
+      });
+    }
+    for (const object of recipe.objects) {
+      nodes.push({
+        id: object.id,
+        name: object.definition.name,
+        value: 1,
+        symbolSize: 10,
+        category: 2,
+      });
+      links.push({
+        source: object.id,
+        target: '2',
+      });
+    }
+    for (const place of recipe.places) {
+      nodes.push({
+        id: place.id,
+        name: place.name,
+        value: 1,
+        symbolSize: 10,
+        category: 3,
+      });
+      links.push({
+        source: place.id,
+        target: '3',
+      });
+    }
+    return {
+      type: 'force',
+      categories: [
+        {
+          name: 'Actors',
+          base: 'Actors',
+        },
+        {
+          name: 'Verbs',
+          base: 'Verbs',
+        },
+        {
+          name: 'Objects',
+          base: 'Objects',
+        },
+        {
+          name: 'Places',
+          base: 'Places',
+        },
+      ],
+      nodes,
+      links,
     };
   }
 }
